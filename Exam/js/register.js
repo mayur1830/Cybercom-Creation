9e7608a6d60cdb8ccc3cdc0e1b48383fbfb5d349
Mayur <mayurchavda1379@gmail.com>
2021-01-21 15:12:43 +0530

    third

--- Exam/js/register.js
+++ Exam/js/register.js
@@ -4,10 +4,25 @@ var Admin_info = {
     pass: document.getElementById('password').value,
     conpass: document.getElementById('cpassword').value,
     city: document.getElementById('city').value,
+    tc: document.getElementById('tc').value,
 
 }
-document.getElementById('register').addEventListener('click', function fun() {
-    alert('Registration Successfully')
-    window.location = "login.html"
 
-})
+document.getElementById('register').addEventListener('click', function fun() {
+
+    var a = document.forms["myForm"]["name"].value
+    var b = document.forms["myForm"]["email"].value
+    var c = document.forms["myForm"]["password"].value
+    var d = document.forms["myForm"]["cpassword"].value
+    var e = document.forms["myForm"]["city"].value
+    var f = document.forms["myForm"]["state"].value
+    var g = document.forms["myForm"]["tc"].value
+    if (a === " " || b === " " || c === " " || d === " " || e === " " || f === " " || g === " ") {
+        alert("All Fields Are Required")
+    } else {
+        alert('Registration Successfully')
+        window.location = "login.html"
+    }
+
+
+}.bind(this))
--- Exam/js/user.js
+++ Exam/js/user.js
@@ -1,17 +1,74 @@
 document.getElementById('adduser').addEventListener('click', function fun() {
     var arr = []
-    if (localStorage.getItem('array')) {
-        arr = JSON.parse(localStorage.getItem('array'));
+    if (localStorage.getItem('user')) {
+        arr = JSON.parse(localStorage.getItem('user'));
 
+    }
+
+    var get_age = function getAge() {
+        var today = new Date();
+        var birthDate = new Date(document.getElementById('dob').value);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
     }
     var obj = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         pass: document.getElementById('password').value,
-        dob: document.getElementById('dob').value
+        dob: document.getElementById('dob').value,
+        age: get_age(),
 
     }
     arr.push(obj)
     console.log(arr)
-    localStorage.setItem("array", JSON.stringify(arr))
+    localStorage.setItem("user", JSON.stringify(arr))
 })
+var x = JSON.parse(localStorage.getItem("user"));
+document.write('<div id="data">')
+document.write('<table border = "1" id = "t1">');
+document.write('<tr>');
+document.write('<th>Name</th> <th>Email</th> <th>Password</th> <th>BirthDate</th> <th>Age</th> <th>Action</th>');
+document.write('</tr>')
+if (x === null) {
+    alert("Records not found!");
+    document.getElementById("t1").style.display = "none";
+
+} else {
+    for (let i = 0; i < x.length; i++) {
+        var index = i;
+        document.write('<tr>')
+        document.write('<td>' + x[i].name + '</td>')
+        document.write('<td>' + x[i].email + '</td>')
+        document.write('<td>' + x[i].pass + '</td>')
+        document.write('<td>' + x[i].dob + '</td>')
+        document.write('<td>' + x[i].age + '</td>')
+        document.write('<td><button id="edit" onclick="Edit(index)">Edit</button><button id="delete" onclick="Delete(index)">Delete</button></td>')
+
+        document.write('</tr>')
+
+
+    }
+}
+document.write('</table>');
+document.write('</div>')
+
+function Edit(index) {
+    document.getElementById("adduser").value = "Update User"
+    var name = document.getElementById("name").value = x[index].name
+    var email = document.getElementById("email").value = x[index].email
+    var password = document.getElementById("password").value = x[index].pass
+    var dob = document.getElementById("dob").value = x[index].dob
+    document.getElementById('t1').x[index].name
+
+
+    //console.log(index)
+
+}
+
+function Delete(index) {
+    document.getElementById("t1").removeChild(tr);
+}
